0ae7e1bd95fbd26db50ac2ac7d638b2f8af3739d
Fredy <fredysqm@gmail.com>
2016-11-14 15:50:17 -0500

    test getFeatureInfo

--- sig/urls.py
+++ sig/urls.py
@@ -6,6 +6,7 @@ from sig.views import *
 
 urlpatterns = [
     url(r'^$', sig_main.as_view(), name='sig_main_url'),
+    url(r'^test/$', sig_main2.as_view(), name='sig_main2_url'),
     #url(r'^error404/$', error404.as_view()),
     #url(r'^error500/$', error500.as_view()),
 ]
