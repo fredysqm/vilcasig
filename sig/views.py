368b6df0bb823df54a8a68d36b09a84f2c0f79ca
Fredy <fredysqm@gmail.com>
2016-11-25 12:49:57 -0500

    avance dev

--- sig/views.py
+++ sig/views.py
@@ -8,3 +8,6 @@ class sig_main(TemplateView):
 
 class sig_main2(TemplateView):
     template_name = 'sig/get_feature_info.html'
+
+class sig_future(TemplateView):
+    template_name = 'sig/future.html'
