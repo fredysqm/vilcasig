486e483b7e36e4e4f384983e1c42d0e59955eaff
Fredy <fredysqm@gmail.com>
2016-12-14 08:58:52 -0500

    rc1: social

--- main/settings/local.py
+++ main/settings/local.py
@@ -5,7 +5,7 @@ SECRET_KEY = '2uUqZu93kq9xv^d7@!+9nwk^n2q@_kloa58dvu7#9e4+yru)-G'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'vilcasig_v2',
+        'NAME': 'vilcasig',
         'USER': 'root',
         'PASSWORD': 'pwd123',
         'HOST': 'localhost',
