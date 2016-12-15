4bf87e78601cd0723d966b56ab35f88218021c9d
Fredy <fredysqm@gmail.com>
2016-12-15 10:44:12 -0500

    test

--- main/settings/local.py
+++ main/settings/local.py
@@ -1,11 +1,11 @@
 from ._devel import *
 
 ALLOWED_HOSTS = ['*']
-SECRET_KEY = '2uUqZu93kq9xv^d7@!+9nwk^n2q@_kloa58dvu7#9e4+yru)-G'
+SECRET_KEY = '2uUqZU73kq9xv^d7@!+9Nwk^n2q@_klOa58dvu7#9e4+yru)-G'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.mysql',
-        'NAME': 'vilcasig',
+        'NAME': 'vilcasig_dev',
         'USER': 'root',
         'PASSWORD': 'pwd123',
         'HOST': 'localhost',
