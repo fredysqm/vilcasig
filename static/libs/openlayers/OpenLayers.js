1d4bbd0f7a96a08c82c61104728a6155af961393
Fredy <fredysqm@gmail.com>
2017-04-06 15:30:28 -0500

    bugfix openlayers2

--- static/libs/openlayers/OpenLayers.js
+++ static/libs/openlayers/OpenLayers.js
@@ -86,20 +86,7 @@ var OpenLayers = {
      * {String} Path to this script
      */
     _getScriptLocation: (function() {
-        var r = new RegExp("(^|(.*?\\/))(OpenLayers[^\\/]*?\\.js)(\\?|$)"),
-            s = document.getElementsByTagName('script'),
-            src, m, l = "";
-        for(var i=0, len=s.length; i<len; i++) {
-            src = s[i].getAttribute('src');
-            if(src) {
-                m = src.match(r);
-                if(m) {
-                    l = m[1];
-                    break;
-                }
-            }
-        }
-        return (function() { return l; });
+        return (function() { return "/static/libs/openlayers/"; });
     })(),
     
     /**
