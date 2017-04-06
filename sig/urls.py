899ac3ad4e49ebac4f208fc6b0e2bcff1de6122c
Fredy <fredysqm@gmail.com>
2017-04-06 18:53:00 -0500

    idioma 100%

--- sig/urls.py
+++ sig/urls.py
@@ -4,8 +4,6 @@ from sig.views import *
 
 urlpatterns = [
     url(r'^$', sig_main.as_view(), name='sig_main_url'),
-    url(r'^get_feature_info/$', sig_main2.as_view(), name='sig_main2_url'),
-    url(r'^future/$', sig_future.as_view(), name='sig_future_url'),
     #url(r'^error404/$', error404.as_view()),
     #url(r'^error500/$', error500.as_view()),
 ]
--- sig/views.py
+++ sig/views.py
@@ -3,9 +3,3 @@ from django.views.generic import TemplateView
 
 class sig_main(TemplateView):
     template_name = 'sig/main.html'
-
-class sig_main2(TemplateView):
-    template_name = 'sig/get_feature_info.html'
-
-class sig_future(TemplateView):
-    template_name = 'sig/future.html'
